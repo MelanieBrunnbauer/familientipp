3b5eae88cf3173ef00e2f4dc62fd99c151f4d430
Melanie Brunnbauer <16886065+MelanieBrunnbauer@users.noreply.github.com>
2018-04-28 22:26:38 +0200

    TIPP-1 initial setup of Spring boot application

---
+++ src/main/java/my/familientipp/app/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package my.familientipp.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hier entsteht unsere Tipp-App für die WM 2018!";
+    }
+
+}
